0e968a1d2d871a77f72e5c8f9fc9d236a93c6916
Marcos Costa <marcoscosta@air-de-marcos.lan>
2024-06-26 12:11:03 +0100

    Complete app

---
+++ AgeInDays/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int anos, meses, dias, valor, resto;
+
+            Console.WriteLine("Insira o valor aqui: ");
+            valor = int.Parse(Console.ReadLine());
+
+            anos = valor / 365;
+            resto = valor % 365;
+
+            meses = resto / 30;
+            dias = resto % 30;
+
+            Console.WriteLine(anos + " anos (s)");
+            Console.WriteLine(meses + " mes (es)");
+            Console.WriteLine(dias + " dia (s)");
+        }
+    }
+}
